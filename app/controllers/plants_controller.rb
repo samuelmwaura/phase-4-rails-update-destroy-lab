7ac86c771f712ab5d2ec268f26d72d53ea03d7e8
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-10-01 17:33:54 +0300

    commit tree 2e2c4ef1603b3ded50c98eadd083336f9f66c337

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,8 +8,10 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_a_plant
     render json: plant
+  rescue ActiveRecord::RecordNotFound
+    send_error
   end
 
   # POST /plants
@@ -18,9 +20,35 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  #PATCH /plants/:id
+  def update
+    plant = find_a_plant
+    plant.update(plant_params)
+    render json: plant,status: :accepted
+    rescue ActiveRecord::RecordNotFound
+    send_error
+  end
+
+#DESTROY /plants/:id
+   def destroy
+      plant = find_a_plant
+      plant.destroy
+      head :no_content
+      rescue ActiveRecord::RecordNotFound
+      send_error
+   end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_a_plant
+    plant = Plant.find(params[:id])
+  end
+
+  def send_error
+    render json: {error:"Plant Not Found!"},status: :not_found
   end
 end
